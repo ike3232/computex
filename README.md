789f76358b4f86020b02bf1f6fdcc27c5cd4ecf2
ike3232 <iketonijon@gmail.com>
2024-04-04 16:15:46 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# computex
+# Containerisation-React-Django-App and Github action pipeline
+files contains, a react frontend, a django backend and a potrgres database
+there is a seperate dockerfile for frontend and backend 
+and also a Github Action pipeline for backend test and publishing to dockerhub
+and another pipeline workflow for building the front end. packaginf the articat and deploying to an ubuntu server in an Amazon instance.
---
+++ main.tf
@@ -0,0 +1,69 @@
+provider "google" {
+  credentials = jsondecode(var.google_credentials)
+  project     = "protean-topic-411511"
+  region      = "us-central1"
+}
+variable "google_credentials" {
+  description = "Google Cloud service account credentials"
+}
+
+
+# Define Google Compute Engine instances
+
+# Application Server
+resource "google_compute_instance" "app_instance" {
+  name         = "app-instance"
+  machine_type = "n1-standard-2"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Include a public IP address
+    }
+  }
+
+  metadata_startup_script = "sudo docker run -d -p 3000:3000 <DOCKER_IMAGE_NAME>"
+}
+
+# Database Server
+resource "google_compute_instance" "db_instance" {
+  name         = "db-instance"
+  machine_type = "n1-standard-2"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Include a public IP address or configure private IP if needed
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    # Install and configure your database server here
+    # Example: Install and configure MySQL
+    sudo apt-get update
+    sudo apt-get install -y mysql-server
+    sudo mysql_secure_installation
+  EOF
+}
+
+# Define firewall rules if necessary
+
+# Output the public IP address of the application server
+output "app_instance_ip" {
+  value = google_compute_instance.app_instance.network_interface[0].access_config[0].nat_ip
+}
