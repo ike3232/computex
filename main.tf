14d2cef899840d1c313914f66612ac8498d8aced
ike3232 <iketonijon@gmail.com>
2024-04-12 15:47:36 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -6,6 +7,7 @@ terraform {
     }
   }
 }
+
 
 provider "google" {
   # Configuration options
