fac3003f387c4bfbf2c97a8f52e0d40a968c1c16
ike3232 <iketonijon@gmail.com>
2024-04-12 16:24:51 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,76 +1,37 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.24.0"
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "my-Instance"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "e2 micro"
+  zone         = "us-central1-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
     }
   }
-}
 
-provider "google" {
-  # Configuration options
-}
-
-resource "google_compute_instance_template" "example" {
-  name          = "my-instance"
-  machine_type  = "e2-micro"
-  
-  disk {
-    source_image = "debian-cloud/debian-10"
-    auto_delete  = true
   }
 
   network_interface {
     network = "default"
+
     access_config {
-      // Ephemeral IP
+      // Ephemeral public IP
     }
   }
 
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install -y nodejs npm && git clone  && cd /home/anthony/computex && npm install && npm start"
-}
 
-resource "google_compute_health_check" "basic" {
-  name               = "basic-check"
-  check_interval_sec = 1
-  timeout_sec        = 1
-  tcp_health_check {
-    port = 80
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
   }
-}
-
-resource "google_compute_instance_group_manager" "example" {
-  name               = "example-instance-group-manager"
-  base_instance_name = "example-instance"
-  zone               = "us-central1-a"
-  target_size        = 1
-
-  auto_healing_policies {
-    health_check      = google_compute_health_check.basic.self_link
-    initial_delay_sec = 60
-  }
-
-  update_policy {
-    type          = "PROACTIVE"
-    minimal_action = "REPLACE"
-  }
-
-  named_port {
-    name = "http"
-    port = 80
-  }
-}
-
-
-
-resource "google_compute_autoscaler" "example_autoscaler" {
-  name               = "example-autoscaler"
-  zone               = "us-central1-a"
-  target             = google_compute_instance_group_manager.example.instance_group
-  autoscaling_policy {
-    min_replicas           = 1
-    max_replicas           = 10
-    cool_down_period_sec   = 60
-    cpu_utilization_target = 0.8
-  }
-}
