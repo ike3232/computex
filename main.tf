aac1e53cd71636850b7d1d5819cafeb0f49c8faf
ike3232 <iketonijon@gmail.com>
2024-04-16 15:40:04 +0100

    first

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "google" {
 resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  zone         = "us-central1-b"
 
   boot_disk {
     initialize_params {
